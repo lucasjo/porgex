f61daf4cf48443f4d0af287775a26eae5c2d15d6
kikimans <kikimans.lucas@gmail.com>
2015-09-23 17:35:49 +0900

    test

--- porgex-agent/example/usage/cpuusage.go
+++ porgex-agent/example/usage/cpuusage.go
@@ -143,7 +143,7 @@ func calculateCPUPercent(previousCpu uint64, v *models.AppCpuStats) float64 {
 
 	var cpuPercnt = 0.0
 
-	cpuPercnt = (previousCpu / v.CPUStats.CPUUsage.TotalUsage) / len(v.CPUStats.CPUUsage.PercpuUsage) * 100.0
+	cpuPercnt = (previousCpu / v.CPUStats.CPUUsage.TotalUsage) / uint64(len(v.CPUStats.CPUUsage.PercpuUsage)) * 100.0
 
 	return cpuPercnt
 
@@ -165,6 +165,10 @@ func main() {
 	time.Sleep(time.Second * 2)
 
 	err := SetCpuUsage(id, cStats)
+
+	if err != nil {
+		fmt.Errorf("error message : ", err)
+	}
 
 	cpuPer := calculateCPUPercent(previousCpu, cStats)
 
